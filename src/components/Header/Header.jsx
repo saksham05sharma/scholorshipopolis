a9ca5aca2127dfa77612705ec8b19e7c0e58d165
akshatmittal61 <akshatmittal2506@gmail.com>
2022-04-23 10:02:28 +0530

    updated header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,204 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import favicon from "../../images/favicon.jpg";
+import * as React from "react";
+import favicon from "../../images/favicon-bg.png";
 import "./header.css";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import { Link } from "react-router-dom";
 
-const Header = () => {
+const pages = [
+	{
+		text: "Home",
+		link: "/",
+	},
+	{
+		text: "About",
+		link: "/about",
+	},
+	{
+		text: "Check Eligibility",
+		link: "/check",
+	},
+	{
+		text: "Login",
+		link: "/login",
+	},
+];
+const settings = [
+	{
+		text: "Account",
+		link: "/account",
+	},
+	{
+		text: "Logout",
+		link: "/logout",
+	},
+];
+
+const ResponsiveAppBar = () => {
+	const [anchorElNav, setAnchorElNav] = React.useState(null);
+	const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+	const handleOpenNavMenu = (event) => {
+		setAnchorElNav(event.currentTarget);
+	};
+	const handleOpenUserMenu = (event) => {
+		setAnchorElUser(event.currentTarget);
+	};
+
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
+
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null);
+	};
+
 	return (
-		<header className="header">
-			<div className="header-left">
-				<Link className="header-left-image" to="/">
-					<img src={favicon} alt="Scholorshipopolis" />
-				</Link>
-			</div>
-			<div className="header-right">
-				<ul className="header-right-nav__ul">
-					<li className="header-right-nav__li">
-						<Link to="/about">About Us</Link>
-					</li>
-					<li className="header-right-nav__li">
-						<Link to="/about">Check Eligibility</Link>
-					</li>
-				</ul>
-			</div>
-		</header>
+		<AppBar position="static">
+			<Container maxWidth="xl">
+				<Toolbar disableGutters>
+					<Typography
+						variant="h6"
+						noWrap
+						component="div"
+						sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+					>
+						<Link className="header-left-image" to="/">
+							<img src={favicon} alt="Scholorshipopolis" />
+						</Link>
+					</Typography>
+
+					<Box
+						sx={{
+							flexGrow: 1,
+							display: { xs: "flex", md: "none" },
+						}}
+					>
+						<IconButton
+							size="large"
+							aria-label="account of current user"
+							aria-controls="menu-appbar"
+							aria-haspopup="true"
+							onClick={handleOpenNavMenu}
+							color="inherit"
+						>
+							<MenuIcon />
+						</IconButton>
+						<Menu
+							id="menu-appbar"
+							anchorEl={anchorElNav}
+							anchorOrigin={{
+								vertical: "bottom",
+								horizontal: "left",
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: "top",
+								horizontal: "left",
+							}}
+							open={Boolean(anchorElNav)}
+							onClose={handleCloseNavMenu}
+							sx={{
+								display: { xs: "block", md: "none" },
+							}}
+						>
+							{pages.map((page) => (
+								<MenuItem
+									key={page}
+									onClick={handleCloseNavMenu}
+								>
+									<Typography textAlign="center">
+										<Link to={page.link}>{page.text}</Link>
+									</Typography>
+								</MenuItem>
+							))}
+						</Menu>
+					</Box>
+					<Typography
+						variant="h6"
+						noWrap
+						component="div"
+						sx={{
+							flexGrow: 1,
+							display: { xs: "flex", md: "none" },
+						}}
+					>
+						<Link className="header-left-image" to="/">
+							<img src={favicon} alt="Scholorshipopolis" />
+						</Link>
+					</Typography>
+					<Box
+						sx={{
+							flexGrow: 1,
+							display: { xs: "none", md: "flex" },
+						}}
+					>
+						{pages.map((page) => (
+							<Button
+								key={page}
+								onClick={handleCloseNavMenu}
+								sx={{ my: 2, color: "white", display: "block" }}
+							>
+								<Link to={page.link}>{page.text}</Link>
+							</Button>
+						))}
+					</Box>
+
+					<Box sx={{ flexGrow: 0 }}>
+						<Tooltip title="Open settings">
+							<IconButton
+								onClick={handleOpenUserMenu}
+								sx={{ p: 0 }}
+							>
+								<Avatar
+									alt="Remy Sharp"
+									src="/static/images/avatar/2.jpg"
+								/>
+							</IconButton>
+						</Tooltip>
+						<Menu
+							sx={{ mt: "45px" }}
+							id="menu-appbar"
+							anchorEl={anchorElUser}
+							anchorOrigin={{
+								vertical: "top",
+								horizontal: "right",
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: "top",
+								horizontal: "right",
+							}}
+							open={Boolean(anchorElUser)}
+							onClose={handleCloseUserMenu}
+						>
+							{settings.map((setting) => (
+								<MenuItem
+									key={setting}
+									onClick={handleCloseUserMenu}
+								>
+									<Typography textAlign="center">
+										<Link to={setting.link}>
+											{setting.text}
+										</Link>
+									</Typography>
+								</MenuItem>
+							))}
+						</Menu>
+					</Box>
+				</Toolbar>
+			</Container>
+		</AppBar>
 	);
 };
-
-export default Header;
+export default ResponsiveAppBar;
